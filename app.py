656bc3fe9ba5aa1d7710074a617b62a2da92546d
Imane Guerouan <imaneguerouan2003@gmail.com>
2025-12-24 15:21:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import streamlit as st
 import pandas as pd
 import joblib
+import os
+
+# Chemin du dossier courant (où se trouve app.py)
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Chargement du modèle et du scaler
-model = joblib.load("extra_trees_model.pkl")
-scaler = joblib.load("scaler.pkl")
+model_path = os.path.join(current_dir, "extra_trees_model.pkl")
+scaler_path = os.path.join(current_dir, "scaler.pkl")
 
+model = joblib.load(model_path)
+scaler = joblib.load(scaler_path)
 
 st.title("Détection d'attaques IoT")
 st.write("Application de classification des types d’attaques réseau IoT")
@@ -19,7 +25,7 @@ if uploaded_file is not None:
     st.subheader("Aperçu des données")
     st.dataframe(df_new.head())
 
-    # Suppression colonne inutile si présente
+    # Supprimer colonne inutile si présente
     if "Unnamed: 0" in df_new.columns:
         df_new = df_new.drop(columns=["Unnamed: 0"])
 
@@ -28,7 +34,6 @@ if uploaded_file is not None:
 
     # Prédiction
     predictions = model.predict(df_scaled)
-
     df_new["Prediction_Attack_type"] = predictions
 
     st.subheader("Résultats de la prédiction")
